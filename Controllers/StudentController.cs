d3b17d5dedf0268a8d754e337629ac9d397bc0e4
Gopal.Nyaupane <gopal.nyaupane100@gmail.com>
2023-04-17 09:04:22 +0545

    update

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StudentManagementSystemN1.Models;
 
 namespace StudentManagementSystemN1.Controllers
 {
     public class StudentController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+        [HttpGet]
         public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student obj)
         {
             return View();
         }
--- Models/Student.cs
+++ Models/Student.cs
@@ -3,5 +3,12 @@
     public class Student
     {
         public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int Gender { get; set; }
+        public int Department { get; set; }
+        public int[] OptSubject { get; set; }
+        public string Message { get; set; }
+
     }
 }
