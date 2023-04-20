fa0d1a3bcbe997d5e8b90e153e39784c31983f61
Gopal.Nyaupane <gopal.nyaupane100@gmail.com>
2023-04-20 08:14:01 +0545

    udpate

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StudentManagementSystemN1.Data;
 using StudentManagementSystemN1.Models;
 
 namespace StudentManagementSystemN1.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public StudentController(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
         [HttpGet]
         public IActionResult Index()
         {
@@ -18,6 +24,21 @@ namespace StudentManagementSystemN1.Controllers
         [HttpPost]
         public IActionResult Create(StudentViewModel obj)
         {
+
+            Student student = new Student();
+            student.Name = obj.Name;
+            student.Email = obj.Email;
+            student.Gender = obj.Gender;  
+            student.Phone = obj.Phone;
+            student.Department = obj.Department;
+            student.Message = obj.Message;
+            _context.Students.Add(student);
+            _context.SaveChanges();
+
+            //StudentOptSubject studentOptSubject = new StudentOptSubject();
+            //studentOptSubject.SubjectName=obj.  
+
+
             return View();
         }
     }
